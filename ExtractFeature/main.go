402fcb7f3e5970a54cf97b67dd983eda31a6d5cc
lookupman <16478489@qq.com>
2020-03-23 16:06:57 +0800

    test

--- ExtractFeature/main.go
+++ ExtractFeature/main.go
@@ -10,8 +10,8 @@ import (
 )
 
 const (
-	device      string = "\\Device\\NPF_{2CCCFA0A-FEE2-4688-BC5A-43A805A8DC67}"
-	//device      string = "ens33"
+	// device      string = "\\Device\\NPF_{2CCCFA0A-FEE2-4688-BC5A-43A805A8DC67}"
+	device      string = "ens33"
 	promiscuous bool   = false //是否开启混杂模式
 )
 
