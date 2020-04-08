34cfa84a2713a95b512404a67eaaf83fadf7cea4
dream-kzx <16478489@qq.com>
2020-04-09 00:01:34 +0800

    与GUI界面初步耦合完成

--- ExtractFeature/main.go
+++ ExtractFeature/main.go
@@ -2,26 +2,96 @@ package main
 
 import (
 	"FlowDetection/CallPredict"
+	"FlowDetection/GUI"
 	"FlowDetection/baseUtil"
 	"FlowDetection/flowFeature"
 	"FlowDetection/sniff"
 	"fmt"
+	"github.com/asticode/go-astikit"
+	"github.com/asticode/go-astilectron"
+	bootstrap "github.com/asticode/go-astilectron-bootstrap"
+	"github.com/pkg/errors"
 	"log"
 	"strconv"
 )
 
 const (
-	// device string = "\\Device\\NPF_{2CCCFA0A-FEE2-4688-BC5A-43A805A8DC67}"
-	device      string = "ens33"
+	device string = "\\Device\\NPF_{2CCCFA0A-FEE2-4688-BC5A-43A805A8DC67}"
+	//device      string = "ens33"
 	promiscuous bool = true //是否开启混杂模式
+)
+
+var (
+	resultToGUIChan chan *GUI.FlowResult
 )
 
 func main() {
 
 	featureChan := make(chan *flowFeature.FlowFeature, 5)
 
+	resultToGUIChan = make(chan *GUI.FlowResult, 10)
+
 	go PredictFLowInFeature(featureChan)
-	snifferAndExtract(featureChan)
+	go snifferAndExtract(featureChan)
+
+	startGUI()
+}
+
+func startGUI() {
+	handler = GUI.NewHandler()
+	manager = GUI.NewManager()
+
+	l := log.New(log.Writer(), log.Prefix(), log.Flags())
+	l.Printf("Running app built at %s\n", BuiltAt)
+
+	if err := bootstrap.Run(bootstrap.Options{
+		Asset:    Asset,
+		AssetDir: AssetDir,
+		AstilectronOptions: astilectron.Options{
+			AppName:            AppName,
+			AppIconDarwinPath:  "resources/icon.icns",
+			AppIconDefaultPath: "resources/icon.png",
+			SingleInstance:     true,
+		},
+		Debug:  *Debug,
+		Logger: l,
+		MenuOptions: []*astilectron.MenuItemOptions{{
+			Label: astikit.StrPtr("File"),
+			SubMenu: []*astilectron.MenuItemOptions{
+				{Role: astilectron.MenuItemRoleReload},
+				{Role: astilectron.MenuItemRoleToggleFullScreen},
+				{Role: astilectron.MenuItemRoleToggleDevTools},
+			},
+		}},
+		OnWait: func(_ *astilectron.Astilectron, ws []*astilectron.Window,
+			_ *astilectron.Menu, _ *astilectron.Tray, _ *astilectron.Menu) error {
+			manager.W = ws[0]
+			go func() {
+				for flowResult := range resultToGUIChan {
+					manager.AddFlow(flowResult)
+					manager.SendHostMessage(flowResult.SrcIP)
+					manager.SendConnectionMessage(flowResult.SrcIP + flowResult.SrcPort)
+				}
+
+			}()
+			return nil
+		},
+		RestoreAssets: RestoreAssets,
+		Windows: []*bootstrap.Window{{
+			Homepage:       "index.html",
+			MessageHandler: handler.HandleMessages,
+			Options: &astilectron.WindowOptions{
+				BackgroundColor: astikit.StrPtr("#2d3e50"),
+				Center:          astikit.BoolPtr(true),
+				Height:          astikit.IntPtr(650),
+				Width:           astikit.IntPtr(950),
+				MinHeight:       astikit.IntPtr(650),
+				MinWidth:        astikit.IntPtr(950),
+			},
+		}},
+	}); err != nil {
+		l.Fatal(errors.Wrap(err, "running bootstrap failed"))
+	}
 }
 
 func snifferAndExtract(featureChan chan *flowFeature.FlowFeature) {
@@ -56,9 +126,9 @@ func PredictFLowInFeature(featureChan chan *flowFeature.FlowFeature) {
 			if write {
 				data := feature.FeatureToString()
 				data += attackList[label] + ","
-				data += ipToString(feature.SrcIP)
+				data += ipToString(feature.SrcIP)+","
 				data += strconv.Itoa(int(feature.SrcPort)) + ","
-				data += ipToString(feature.DstIP)
+				data += ipToString(feature.DstIP)+","
 				data += strconv.Itoa(int(feature.DstPort))
 				data += "\n"
 				// log.Println(data)
@@ -70,6 +140,12 @@ func PredictFLowInFeature(featureChan chan *flowFeature.FlowFeature) {
 			log.Println(feature.DstPort, "   ", feature.DstIP)
 			log.Println(feature.FeatureToString())
 
+			flowResult := new(GUI.FlowResult)
+			flowResult.SrcIP = ipToString(feature.SrcIP)
+			flowResult.SrcPort = strconv.Itoa(int(feature.SrcPort))
+			flowResult.AttackType = attackList[label]
+
+			resultToGUIChan <- flowResult
 		}
 
 	}
@@ -80,6 +156,6 @@ func ipToString(ip [4]byte) string {
 	data += strconv.Itoa(int(ip[0])) + "."
 	data += strconv.Itoa(int(ip[1])) + "."
 	data += strconv.Itoa(int(ip[2])) + "."
-	data += strconv.Itoa(int(ip[3])) + ","
+	data += strconv.Itoa(int(ip[3]))
 	return data
 }
